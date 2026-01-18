0addb9e4aa4165a6b22085c9d1fc3399a5b7cf64
bokken <negibokken@gmail.com>
2026-01-18 18:18:47 +0900

    Section 9.8

--- browser/main.py
+++ browser/main.py
@@ -69,6 +69,7 @@ class JSContext:
         self.interp.export_function("log", print)
         self.interp.export_function("querySelectorAll", self.querySelectorAll)
         self.interp.export_function("getAttribute", self.getAttribute)
+        self.interp.export_function("innerHTML_set", self.innerHTML_set)
         self.node_to_handle = {}
         self.handle_to_node = {}
 
@@ -96,6 +97,15 @@ class JSContext:
     def dispatch_event(self, type, elt):
         handle = self.node_to_handle.get(elt, -1)
         self.interp.evaljs(EVENT_DISPATCH_JS, type=type, handle=handle)
+
+    def innerHTML_set(self, handle, s):
+        doc = HTMLParser("<html><body>" + s + "</body></html>").parse()
+        new_nodes = doc.children[0].children
+        elt = self.handle_to_node[handle]
+        elt.children = new_nodes
+        for child in elt.children:
+            child.parent = elt
+        self.tab.render()
 
     def run(self, script, code):
         try:
