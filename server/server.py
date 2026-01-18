c1f4db76f0bf25ef773f8e7f3709496198f402e6
bokken <negibokken@gmail.com>
2026-01-18 16:24:26 +0900

    Section 8.7

--- server/server.py
+++ server/server.py
@@ -1,4 +1,51 @@
 import socket
+import urllib.parse
+
+
+ENTRIES = ["Pavel was here"]
+
+
+def do_request(method, url, headers, body):
+    if method == "GET" and url == "/":
+        return "200 OK", show_comments()
+    elif method == "POST" and url == "/add":
+        params = form_decode(body)
+        return "200 OK", add_entry(params)
+    else:
+        return "404 Not Found", not_found(url, method)
+
+
+def show_comments():
+    out = "<!doctype html>"
+    for entry in ENTRIES:
+        out += "<p>" + entry + "</p>"
+    out += "<form action=add method=post>"
+    out += "<p><input name=guest></p>"
+    out += "<p><button>Sign the book!</button></p>"
+    out += "</form>"
+    return out
+
+
+def form_decode(body):
+    params = {}
+    for field in body.split("&"):
+        name, value = field.split("=", 1)
+        name = urllib.parse.unquote_plus(name)
+        value = urllib.parse.unquote_plus(value)
+        params[name] = value
+    return params
+
+
+def not_found(url, method):
+    out = "<!doctype html>"
+    out += "<h1>{} {} not found!</h1>".format(method, url)
+    return out
+
+
+def add_entry(params):
+    if "guest" in params:
+        ENTRIES.append(params["guest"])
+    return show_comments()
 
 
 def handle_connection(conx):
